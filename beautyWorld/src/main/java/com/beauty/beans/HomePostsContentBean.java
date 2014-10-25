66dfefd02c816d02f425869efe58582ae1c8ad5d
vinay <vinay@192.168.2.18>
2014-10-24 20:20:31 -0500

    new file

---
+++ beautyWorld/src/main/java/com/beauty/beans/HomePostsContentBean.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package main.java.com.beauty.beans;
+
+/**
+ * @author vinay
+ * 
+ */
+public class HomePostsContentBean {
+	private Long postId;
+	private String postTitle;
+	private String postType;
+	private String postDescription;
+	private String postPictureUrl;
+	private String postVidUrl;
+	private String postVideoDesc;
+
+	public Long getPostId() {
+		return postId;
+	}
+
+	public void setPostId(Long postId) {
+		this.postId = postId;
+	}
+
+	public String getPostTitle() {
+		return postTitle;
+	}
+
+	public void setPostTitle(String postTitle) {
+		this.postTitle = postTitle;
+	}
+
+	public String getPostType() {
+		return postType;
+	}
+
+	public void setPostType(String postType) {
+		this.postType = postType;
+	}
+
+	public String getPostDescription() {
+		return postDescription;
+	}
+
+	public void setPostDescription(String postDescription) {
+		this.postDescription = postDescription;
+	}
+
+	public String getPostPictureUrl() {
+		return postPictureUrl;
+	}
+
+	public void setPostPictureUrl(String postPictureUrl) {
+		this.postPictureUrl = postPictureUrl;
+	}
+
+	public String getPostVidUrl() {
+		return postVidUrl;
+	}
+
+	public void setPostVidUrl(String postVidUrl) {
+		this.postVidUrl = postVidUrl;
+	}
+
+	public String getPostVideoDesc() {
+		return postVideoDesc;
+	}
+
+	public void setPostVideoDesc(String postVideoDesc) {
+		this.postVideoDesc = postVideoDesc;
+	}
+
+}
